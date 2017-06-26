27ca7cc94965687e3f40b406034615b75d35e5e2
kazenetu <rakudafactory@gmail.com>
2017-06-26 22:53:35 +0900

    別Exeから呼ばれる実行ファイルを追加

---
+++ ExeProject/MainForm.vb
@@ -0,0 +1,39 @@
+﻿Imports LanguageUtil
+
+Public Class MainForm
+    ''' <summary>
+    ''' 確認メッセージ
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property confirmMessage As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim message As String = String.Format(confirmMessage, "newExe")
+
+        MessageBox.Show(message)
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim languageNmae As String = LanguageResourceUtil.LanguageResources.Japanese.ToString()
+
+        Dim args = Environment.GetCommandLineArgs()
+        If args.Length > 1 Then
+            languageNmae = args(1)
+        End If
+        Dim resource = LanguageResourceUtil.GetInstance().ConvertStringToLanguageResource(languageNmae)
+        LanguageResourceUtil.GetInstance().LoadLanguageResource(resource)
+
+        ' リソースの値をフォームのプロパティに設定
+        SetProperties()
+    End Sub
+
+    Private Sub SetProperties()
+        ' フォームにリソースの値を設定
+        Dim resource = LanguageResourceUtil.getInstance()
+        Me.confirmMessage = resource.GetString(ResourceKeys.Keys.MainForm_確認)
+        Me.Text = resource.GetString(ResourceKeys.Keys.MainForm_タイトル)
+        Me.Label1.Text = resource.GetString(ResourceKeys.Keys.MainForm_ラベル1)
+        Me.Label2.Text = resource.GetString(ResourceKeys.Keys.MainForm_ラベル2)
+
+    End Sub
+End Class
