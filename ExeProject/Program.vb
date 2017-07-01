3dda32c1fd619fdbe40488fe5684a833f00291b2
kazenetu <rakudafactory@gmail.com>
2017-07-02 00:47:17 +0900

    ファイル追加し忘れ対応

---
+++ ExeProject/Program.vb
@@ -0,0 +1,16 @@
+﻿Imports LanguageUtil
+
+Public Class Program
+    ''' <summary>
+    ''' エントリメソッド
+    ''' </summary>
+    Shared Sub Main()
+        ' 連携ファイル読み出し
+        Dim languageName = LanguageResourceUtil.GetInstance().ReadLanguageFile()
+
+        Dim resource = LanguageResourceUtil.GetInstance().ConvertStringToLanguageResource(languageName)
+        LanguageResourceUtil.GetInstance().LoadLanguageResource(resource)
+
+        Application.Run(New MainForm())
+    End Sub
+End Class
