c45670b310caa88e6e7871031ef6d57547d4d130
kazenetu <rakudafactory@gmail.com>
2017-07-02 00:42:46 +0900

    リソースIDのコード体系変更に伴う修正

--- ExeProject/MainForm.vb
+++ ExeProject/MainForm.vb
@@ -2,37 +2,23 @@
 Imports LanguageUtil
 
 Public Class MainForm
-    ''' <summary>
-    ''' 確認メッセージ
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property confirmMessage As String
+    Inherits BaseFrom
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim message As String = String.Format(confirmMessage, "newExe")
+    Public Sub New()
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+        MyBase.formId = "EXEMainForm"
+
+        ' リソースの値をフォームのプロパティに設定
+        MyBase.windowInitialize(Me)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles F0003.Click
+        Dim message As String = String.Format(MyBase.getString("M0001"), "aaa")
 
         MessageBox.Show(message)
     End Sub
 
-    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-
-        ' 連携ファイル読み出し
-        Dim languageName = LanguageResourceUtil.GetInstance().ReadLanguageFile()
-
-        Dim resource = LanguageResourceUtil.GetInstance().ConvertStringToLanguageResource(languageName)
-        LanguageResourceUtil.GetInstance().LoadLanguageResource(resource)
-
-        ' リソースの値をフォームのプロパティに設定
-        SetProperties()
-    End Sub
-
-    Private Sub SetProperties()
-        ' フォームにリソースの値を設定
-        Dim resource = LanguageResourceUtil.GetInstance()
-        Me.confirmMessage = resource.GetString(ResourceKeys.Keys.MainForm_確認)
-        Me.Text = resource.GetString(ResourceKeys.Keys.MainForm_タイトル)
-        Me.Label1.Text = resource.GetString(ResourceKeys.Keys.MainForm_ラベル1)
-        Me.Label2.Text = resource.GetString(ResourceKeys.Keys.MainForm_ラベル2)
-
-    End Sub
 End Class
